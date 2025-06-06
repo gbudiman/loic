39d7712b34d7daa14bb1b9dd24e6771af7debad9
Gloria Budiman <wahyu.g@gmail.com>
2025-06-06 16:51:01 -0400

    Low-orbit ion cannon

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,114 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+type TRequestResult = {
+	startsAt: number;
+	workerId: number;
+	requestId: number;
+	sequenceId: string;
+	successful: boolean;
+	status: number;
+	result: unknown;
+}
+
+interface Env {
+	target_url: string;
+	service_token: string;
+	loic_token: string;
+}
+
+const commonParams = ({ url }: { url: URL }) => ({
+	token: url.searchParams.get('token'),
+	sequenceId: url.searchParams.get('sequence_id') ?? crypto.randomUUID(),
+	workerId: Number(url.searchParams.get('worker_id')),
+	requestsPerWorker: parseInt(url.searchParams.get('requests_per_worker') ?? '2'),
+	fanoutCount: parseInt(url.searchParams.get('fanout') ?? '2'),
+	selfUrl: url.origin + url.pathname,
+})
+
+const executeAsParent = async({ url }: { url: URL }) => {
+	const { fanoutCount, requestsPerWorker, token, sequenceId, selfUrl } = commonParams({ url })
+	const launches = Array.from({ length: fanoutCount}, (_, i) =>
+		fetch(
+			`${selfUrl}?mode=child&requests_per_worker=${requestsPerWorker}&token=${token}&worker_id=${i}&sequence_id=${sequenceId}`, 
+			{ method: 'POST' }
+		).then(async res => {
+			const workerResult: { requestResults: TRequestResult[] } = await res.json()
+
+			return {
+				workerId: i,
+				workerResult
+			}
+		})
+	)
+
+	const sessionResults = await Promise.all(launches)
+	const flattenedResults = sessionResults.map(worker => worker.workerResult.requestResults).flat()
+	return new Response(JSON.stringify({
+		requestsSucceeded: flattenedResults.filter(r => r.successful).length,
+		requestsFailed: flattenedResults.filter(r => !r.successful).length,
+		totalRequests: flattenedResults.length,
+		flattenedResults: flattenedResults.sort((a, b) => a.startsAt - b.startsAt),
+	}))
+}
+
+const executeAsChild = async({ url, env }: { url: URL, env: Env }) => {
+	const { workerId, requestsPerWorker, sequenceId } = commonParams({ url })
+	const launches = Array.from({ length: requestsPerWorker }, (_, i) => {
+		const startsAt = performance.now()
+
+		return fetch(env.target_url, {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+				"X-Request-ID": String(i),
+				"X-Worker-ID": String(workerId),
+				"X-Sequence-ID": sequenceId,
+				"X-LOIC-Service-Token": env.loic_token,
+			}
+		}).then(async res => {
+			const result = await res.json()
+
+			return {
+				startsAt,
+				workerId: workerId,
+				requestId: i,
+				sequenceId: sequenceId,
+				successful: res.ok,
+				status: res.status,
+				result: result
+			} as TRequestResult
+		}).catch(err => {
+			return {
+				startsAt,
+				workerId: workerId,
+				requestId: i,
+				sequenceId: sequenceId,
+				successful: false,
+				status: err.status,
+				result: err.message
+			} as TRequestResult
+		})
+	})
+
+	const requestResults = await Promise.all(launches)
+	return new Response(JSON.stringify({
+		requestResults
+	}))
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url)
+		const token = url.searchParams.get('token')
+		const mode = url.searchParams.get('mode')
+		
+		if (token !== env.service_token) {
+			return new Response(JSON.stringify({
+				error: 'Service Token Required'
+			}))
+		}
+
+		return (mode === 'child') ? executeAsChild({url, env}) : executeAsParent({ url })
 	},
 } satisfies ExportedHandler<Env>;
